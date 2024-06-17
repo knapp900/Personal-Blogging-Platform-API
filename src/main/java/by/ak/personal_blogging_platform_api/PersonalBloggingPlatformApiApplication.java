cc0c010f4128499d92a73ee824110eded3972ed3
Arthur Knapp <knapp900@gmail.com>
2024-06-17 13:43:40 +0300

    Created first commit

---
+++ src/main/java/by/ak/personal_blogging_platform_api/PersonalBloggingPlatformApiApplication.java
@@ -0,0 +1,13 @@
+package by.ak.personal_blogging_platform_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalBloggingPlatformApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalBloggingPlatformApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/ak/personal_blogging_platform_api/PersonalBloggingPlatformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package by.ak.personal_blogging_platform_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBloggingPlatformApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
