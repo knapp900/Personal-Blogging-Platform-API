ed9e6bbe0dba8d83dfa86276568d85b3c138d55a
Arthur Knapp <knapp900@gmail.com>
2024-06-18 17:09:57 +0300

    added user controller

--- src/main/java/by/ak/personal_blogging_platform_api/controller/UserController.java
+++ src/main/java/by/ak/personal_blogging_platform_api/controller/UserController.java
@@ -1,5 +1,19 @@
 package by.ak.personal_blogging_platform_api.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
 
+	/*
+	 * Create
+	 * Get
+	 * Update
+	 * Delete
+	 */
+	
+	
+	
 }
